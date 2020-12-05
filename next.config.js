00865e6ab78ffce79e27a063f3f09041f674edc7
luke-h1 <luke.howsam@yahoo.com>
2020-12-05 20:42:04 +0000

    next plugins

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const {
-  PHASE_PRODUCTION_BUILD,
-  PHASE_DEVELOPMENT_SERVER,
-} = require('next/constants');
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    console.log('IM IN DEV MODE ');
-    return defaultConfig;
-  }
-  return defaultConfig;
-};
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
+
+dotenvLoad();
+const withEnv = nextEnv();
+module.exports = withEnv();
