918c068ddd40765997525162e927254e4cb18dfc
luke-h1 <luke.howsam@yahoo.com>
2020-12-05 11:28:43 +0000

    next router

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import Link from 'next/link';
-const Page = () => (
-  <div>
-    <h1>Note index page</h1>
-    <Link href='/notes/[id]' as={`/notes/1`}>
-      <a>Note 1</a>
-    </Link>
-  </div>
-);
-export default Page;
+import React from 'react'
+import Link from 'next/link'
+
+export default () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {notes.map(note => (
+        <div>
+          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
+}
