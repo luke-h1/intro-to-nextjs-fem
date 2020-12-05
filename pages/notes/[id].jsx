e9460785b061cd146006b35db068dabaf60bbc82
luke-h1 <luke.howsam@yahoo.com>
2020-12-05 20:15:08 +0000

    variants & styling

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+// pages/[id].jsx
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
-const Page = () => {
-  const router = useRouter();
+export default () => {
+  const router = useRouter()
+  const { id }= router.query
 
-  const { id } = router.query;
-  console.log(id)
-
-  return ( 
-    <h1>
-      Note {id}
-    </h1>
+  return (
+    <div sx={{variant: 'containers.page'}}>
+      <h1>Note: {id} </h1>
+    </div>
   )
-};
-export default Page;
+}
