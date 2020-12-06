aeb60c24d71bc0a8b1425517708108b90730e8b3
luke-h1 <luke.howsam@yahoo.com>
2020-12-06 18:59:29 +0000

    getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 import { jsx } from 'theme-ui';
 import Link from 'next/link';
 
-export default () => (
+export default ({ content }) => (
   <div sx={{ height: `calc(100vh - 60px)` }}>
     <div
       sx={{
@@ -12,16 +12,18 @@ export default () => (
         height: '100%',
       }}
     >
-      <h1 sx={{ fontSize: 8, my: 0 }}>
-        This is a really dope note taking app.
-      </h1>
+      <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
     </div>
   </div>
 );
 
-// export function getStaticProps(context) {
-//   console.log(context);
-//   return {
-//     props: {},
-//   };
-// }
+export function getStaticProps() {
+  // get data from CMS
+  return {
+    props: {
+      content: {
+        title: 'this is my really nice app',
+      },
+    },
+  };
+}
--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,16 +1,28 @@
 // pages/[id].jsx
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
+import { jsx } from 'theme-ui';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-export default () => {
-  const router = useRouter()
-  const { id }= router.query
-
+export default ({note}) => {
   return (
-    <div sx={{variant: 'containers.page'}}>
-      <h1>Note: {id} </h1>
+    <div sx={{ variant: 'containers.page' }}>
+      <h1>Note: {note.title} </h1>
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps({ params, req, res }) {
+  const response = await fetch(`http://localhost:3000/api/note/${params.id}`);
+  if (!response.ok) {
+    res.writeHead(302, { Location: '/notes' });
+    res.end();
+    return {
+      props: {},
+    };
+  }
+  const { data } = await response.json();
+  return {
+    props: { note: data },
+  };
 }
